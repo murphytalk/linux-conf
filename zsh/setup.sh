dd0247d2c5db07c144246aef5dc063dc39849356
Mu Lu <murphytalk@gmail.com>
2020-04-29 12:44:01 +0900

    expand script path

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
-which zsh > /dev/null
+#which zsh > /dev/null
+P=$( cd "$(dirname $0)" >/dev/null 2>&1 ; pwd -P )
 if [ $? -ne 0 ];then
     echo Install zsh first
     exit 1
 fi
 cd ~
-[ -d .zprezto ] && exit 1
-
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-for i in `dirname $0`/.z*;do ln -s $i;done
-zsh `dirname $0`/setup.zsh
-
+if [ ! -d .zprezto ];then
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+fi
+for i in $P/.z*;do
+    f=$HOME/$(basename $i)
+    [ -L  $f ] && rm -f $f
+    ln -s $i $f
+done
+zsh $P/setup.zsh
--- zsh/setup.zsh
+++ zsh/setup.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 setopt EXTENDED_GLOB
+P=$( cd "$(dirname $0)" >/dev/null 2>&1 ; pwd -P )
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     HOME_FILE="${ZDOTDIR:-$HOME}/.${rcfile:t}"
     if [ -z "$HOME_FILE" ];then
@@ -7,7 +8,7 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     fi
     [[ ! -L  $HOME_FILE ]] && ln -s "$rcfile" $HOME_FILE
     #override with my own zsh files
-    MYFILE=`dirname $0`/.${rcfile:t}
-    [[  -f $MYFILE ]] && rm -f $HOME_FILE && ln -s $MYFILE $HOME_FILE
+    MYFILE=$P/.${rcfile:t}
+    [[  -f $Mrm YFILE ]] && rm -f $HOME_FILE && ln -s $MYFILE $HOME_FILE
 done
 
